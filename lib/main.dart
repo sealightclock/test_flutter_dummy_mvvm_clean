bdea550b62270f5aad781a90112274f7f507a4c1
Sealight Clock <chbebram@hotmail.com>
2025-03-16 00:08:46 -0700

    6th commit: ViewModel should be handled by View, but not by MyApp.

--- lib/main.dart
+++ lib/main.dart
@@ -1,43 +1,21 @@
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
-import 'package:test_flutter_dummy_mvvm_clean/data/local/my_string_shared_prefs_repository.dart';
-import 'package:test_flutter_dummy_mvvm_clean/data/remote/my_string_backend_server_repository.dart';
-import 'package:test_flutter_dummy_mvvm_clean/domain/usecase/local/get_my_string_from_shared_prefs_use_case.dart';
-import 'package:test_flutter_dummy_mvvm_clean/domain/usecase/local/store_my_string_to_shared_prefs_use_case.dart';
-import 'package:test_flutter_dummy_mvvm_clean/domain/usecase/remote/get_my_string_from_backend_server_use_case.dart';
-import 'package:test_flutter_dummy_mvvm_clean/presentation/view/my_string_home_screen.dart';
-import 'package:test_flutter_dummy_mvvm_clean/presentation/viewmodel/my_string_viewmodel.dart';
+import 'presentation/view/my_string_home_screen.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key}); // Fix: Added key parameter
+  const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    final sharedPrefsRepository = MyStringSharedPrefsRepository();
-    final backendServerRepository = MyStringBackendServerRepository();
-
-    return MultiProvider(
-      providers: [
-        ChangeNotifierProvider<MyStringViewModel>(
-          create: (_) => MyStringViewModel(
-      getLocalUseCase: GetMyStringFromSharedPrefsUseCase(repository: sharedPrefsRepository),
-      storeLocalUseCase: StoreMyStringToSharedPrefsUseCase(repository: sharedPrefsRepository),
-      getRemoteUseCase: GetMyStringFromBackendServerUseCase(repository: backendServerRepository),
-    ),
-
-    ),
-        ],
-      child: MaterialApp(
-        title: 'Test Flutter Dummy MVVM Clean',
-        theme: ThemeData(
-          primarySwatch: Colors.blue,
-        ),
-        home: MyStringHomeScreen(),
+    return MaterialApp(
+      title: 'Test Flutter Dummy MVVM Clean',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
       ),
+      home: const MyStringHomeScreen(),
     );
   }
 }
--- lib/presentation/view/my_string_home_screen.dart
+++ lib/presentation/view/my_string_home_screen.dart
@@ -1,41 +1,55 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'package:test_flutter_dummy_mvvm_clean/presentation/viewmodel/my_string_viewmodel.dart';
+import '../../data/local/my_string_shared_prefs_repository.dart';
+import '../../data/remote/my_string_backend_server_repository.dart';
+import '../../domain/usecase/local/get_my_string_from_shared_prefs_use_case.dart';
+import '../../domain/usecase/local/store_my_string_to_shared_prefs_use_case.dart';
+import '../../domain/usecase/remote/get_my_string_from_backend_server_use_case.dart';
+import '../viewmodel/my_string_viewmodel.dart';
 
 class MyStringHomeScreen extends StatelessWidget {
-  const MyStringHomeScreen({super.key}); // Fix: Added key parameter
+  const MyStringHomeScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    final viewModel = Provider.of<MyStringViewModel>(context);
-
-    return Scaffold(
-      appBar: AppBar(title: Text('Test Flutter Dummy MVVM Clean')),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          children: [
-            TextField(
-              controller: viewModel.textController,
-              decoration: InputDecoration(labelText: 'Enter a string'),
+    return ChangeNotifierProvider(
+      create: (_) => MyStringViewModel(
+        getLocalUseCase: GetMyStringFromSharedPrefsUseCase(repository: MyStringSharedPrefsRepository()),
+        storeLocalUseCase: StoreMyStringToSharedPrefsUseCase(repository: MyStringSharedPrefsRepository()),
+        getRemoteUseCase: GetMyStringFromBackendServerUseCase(repository: MyStringBackendServerRepository()),
+      ),
+      child: Consumer<MyStringViewModel>(
+        builder: (context, viewModel, child) {
+          return Scaffold(
+            appBar: AppBar(title: const Text('Test Flutter Dummy MVVM Clean')),
+            body: Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Column(
+                children: [
+                  TextField(
+                    controller: viewModel.textController,
+                    decoration: const InputDecoration(labelText: 'Enter a string'),
+                  ),
+                  const SizedBox(height: 10),
+                  ElevatedButton(
+                    onPressed: viewModel.updateFromUser,
+                    child: const Text('Update from User'),
+                  ),
+                  const SizedBox(height: 10),
+                  ElevatedButton(
+                    onPressed: viewModel.updateFromServer,
+                    child: const Text('Update from Server'),
+                  ),
+                  const SizedBox(height: 20),
+                  Text(
+                    'Current Value: ${viewModel.myString}',
+                    style: const TextStyle(fontSize: 18),
+                  ),
+                ],
+              ),
             ),
-            SizedBox(height: 10),
-            ElevatedButton(
-              onPressed: () => viewModel.updateFromUser(),
-              child: Text('Update from User'),
-            ),
-            SizedBox(height: 10),
-            ElevatedButton(
-              onPressed: () => viewModel.updateFromServer(),
-              child: Text('Update from Server'),
-            ),
-            SizedBox(height: 20),
-            Text(
-              'Current Value: ${viewModel.myString}',
-              style: TextStyle(fontSize: 18),
-            ),
-          ],
-        ),
+          );
+        },
       ),
     );
   }
